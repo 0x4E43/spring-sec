acf7188f0b0b88af3ed2209448f4f532b766e20a
0x4E43 <nimaic.dev@gmail.com>
2023-12-31 00:11:51 +0530

    intial commit | Allow all request

---
+++ src/main/java/com/springsecurity/security/SecurityApplication.java
@@ -0,0 +1,20 @@
+package com.springsecurity.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+	@GetMapping
+	public String test(){
+		return "Hello World";
+	}
+}
---
+++ src/main/java/com/springsecurity/security/securityConfig/SecConfig.java
@@ -0,0 +1,24 @@
+package com.springsecurity.security.securityConfig;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecConfig {
+
+
+    @Bean
+    SecurityFilterChain doFilter(HttpSecurity http) throws Exception{
+        //allow all endPoint
+        http.authorizeHttpRequests(
+                (auth) -> auth.requestMatchers("/**").permitAll()
+        );
+        return http.build();
+    }
+}
