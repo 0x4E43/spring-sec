408f751f0c65c2d56e941174c132c5ad95838907
0x4E43 <nimaic.dev@gmail.com>
2024-02-14 20:43:03 +0530

    Basic auth with hard coded password and InMemoryUserDetails Manager

--- src/main/java/com/springsecurity/security/securityConfig/SecConfig.java
+++ src/main/java/com/springsecurity/security/securityConfig/SecConfig.java
@@ -1,12 +1,20 @@
 package com.springsecurity.security.securityConfig;
 
 
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,4 +32,21 @@ public class SecConfig {
         ).httpBasic(Customizer.withDefaults());
         return http.build();
     }
+
+    //To use custom user and password
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}user")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}admin")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
 }
