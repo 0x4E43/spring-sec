7a2c8c615e5e808c8094173dc07cdeaeab88c62b
0x4E43 <nimaic.dev@gmail.com>
2024-02-17 21:40:21 +0530

    Somehow working

---
+++ src/main/java/com/springsecurity/security/securityConfig/AuthProvider.java
@@ -0,0 +1,49 @@
+package com.springsecurity.security.securityConfig;
+
+import com.springsecurity.security.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class AuthProvider implements AuthenticationProvider {
+
+    @Autowired
+    private  UserService userService;
+
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        // Perform your custom authentication logic here
+        // Retrieve user details from userDetailsService and validate the credentials
+        // You can throw AuthenticationException if authentication fails
+        // Example: retrieving user details by username from UserDetailsService
+        UserDetails userDetails = userService.loadUserByUsername(username);
+        if (userDetails == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        // Example: validating credentials
+        if (!password.equals(userDetails.getPassword())) {
+            throw new AuthenticationException("Invalid credentials") {};
+        }
+        // Create a fully authenticated Authentication object
+        Authentication authenticated = new UsernamePasswordAuthenticationToken(
+                userDetails, password, userDetails.getAuthorities());
+        return authenticated;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return  authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
--- src/main/java/com/springsecurity/security/securityConfig/SecConfig.java
+++ src/main/java/com/springsecurity/security/securityConfig/SecConfig.java
@@ -1,17 +1,24 @@
 package com.springsecurity.security.securityConfig;
 
 
+import com.springsecurity.security.user.UserService;
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -21,9 +28,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecConfig {
 
+    @Autowired
+    private AuthProvider authProvider;
+
+    @Autowired
+    private UserService userService;
+
 
     @Bean
-    SecurityFilterChain doFilter(HttpSecurity http) throws Exception{
+    SecurityFilterChain doFilter(HttpSecurity http) throws Exception {
         //allow all endPoint
         http.authorizeHttpRequests(
                 (auth) -> auth
@@ -35,18 +48,23 @@ public class SecConfig {
 
     //To use custom user and password
     @Bean
-    public UserDetailsService users() {
-        UserDetails user = User.builder()
-                .username("user")
-                .password("{noop}user")
-                .roles("USER")
-                .build();
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password("{noop}admin")
-                .roles("USER", "ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
 }
